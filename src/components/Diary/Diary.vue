826af9cf4022d6cd850cd68245b8a80156f60db4
YagaMeow <1850473124@qq.com>
2026-04-07 20:07:51 +0800

    0407

---
+++ src/components/Diary/Diary.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="diary-container _fullscreen">
+    <div class="content">
+      <span> 很久很久以前...</span>
+    </div>
+    <div class="add">
+      <svg xmlns="http://www.w3.org/2000/svg"></svg>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+defineOptions({
+  name: 'MyDiary',
+})
+</script>
+<style lang="scss" scoped></style>
